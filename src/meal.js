ac45f4b7ecdb4902c6d32b98c5c0b5b1a0d15886
Katie Ammon <katie.ammon10@gmail.com>
2021-08-29 19:20:32 -0600

    add code

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
 
+
+
+function nameMenuItem(foodItem) {
+return 'Delicious ' + foodItem;
+
+ }
+
+function createMenuItem(item, cost, meal) {
+  return menuItem = {
+    name: item,
+    price: cost,
+    type: meal,
+  };
 }
 
+function addIngredients(list, ingredients) {
+  if (!ingredients.includes(list)) {
+    return ingredients.push(list);
+  }
+}
+
+function formatPrice(price, dollar) {
+dollar = '$';
+return dollar + price;
+}
+
+function decreasePrice(reducePrice) {
+  var reduce = menuItem.price - (menuItem.price * .1);
+  return reduce;
+}
+
+function createRecipe(name, containing, meal) {
+  return recipe = {
+    title: name,
+    ingredients: containing,
+    type: meal,
+  };
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
--- src/order.js
+++ src/order.js
@@ -1,7 +1,31 @@
+function takeOrder(orders, delivery)  {
+  if (delivery.length < 3) {
+    return delivery.push(orders);
+  }
+}
+function refundOrder(orderNo, deliveries) {
+  for (var i = 0; i < deliveries.length; i++) {
+    if (deliveries[i].orderNumber === orderNo) {
+      return deliveries.splice(i, 1)
+    }
 
+  }
+}
+function listItems(orders) {
+  var food = [];
+  for (var i = 0; i < orders.length; i++) {
+     food.push(orders[i].item)
+    }
+     orders.toString()
+     return orders
+}
+
+// function searchOrder() {
+//
+// }
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
 }
